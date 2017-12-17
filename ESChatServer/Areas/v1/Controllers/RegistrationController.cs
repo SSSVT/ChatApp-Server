b5b87fb8dc7c7be40508d3914cc08874debf2589
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2017-12-17 11:36:51 +0100

    Registration added

--- ESChatServer/Areas/v1/Controllers/RegistrationController.cs
+++ ESChatServer/Areas/v1/Controllers/RegistrationController.cs
@@ -1,11 +1,11 @@
 ﻿using ESChatServer.Areas.v1.Models.Application.Entities;
+using ESChatServer.Areas.v1.Models.Application.Objects;
 using ESChatServer.Areas.v1.Models.Database;
 using ESChatServer.Areas.v1.Models.Database.Interfaces;
 using ESChatServer.Areas.v1.Models.Database.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Entities = ESChatServer.Areas.v1.Models.Database.Entities;
 
@@ -13,21 +13,21 @@ namespace ESChatServer.Areas.v1.Controllers
 {
     [Produces("application/json")]
     [Area("v1")]
-    public class UsersController : Controller
+    public class RegistrationController : Controller
     {
         #region Fields
         protected readonly IUsersRepository _usersRepository;
         protected readonly DatabaseContext _databaseContext;
         #endregion
 
-        public UsersController(DatabaseContext context)
+        public RegistrationController(DatabaseContext context)
         {
             this._databaseContext = context;
             this._usersRepository = new UsersRepository(context);
-        }        
+        }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpPost]
+        public IActionResult Register([FromBody]Registration registration)
         {
             try
             {
@@ -36,15 +36,74 @@ namespace ESChatServer.Areas.v1.Controllers
                     return BadRequest(ModelState);
                 }
 
-                ICollection<Entities.User> result = this._usersRepository.FindAll();
-                if (result != null)
+                string salt = PasswordFactory.GenerateSalt(128);
+                Entities.User user = new Entities.User()
                 {
-                    return Ok(result);
-                }
-                else
+                    FirstName = registration.FirstName,
+                    MiddleName = registration.MiddleName,
+                    LastName = registration.LastName,
+                    Birthday = registration.Birthday,
+                    Gender = registration.Gender,
+                    Username = registration.Username,
+                    PasswordHash = PasswordFactory.Hash(registration.Password, salt),
+                    PasswordSalt = salt,
+                    UTCRegistrationDate = DateTime.UtcNow
+                };
+                this._usersRepository.Add(user, true);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //TODO: SaveException
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync([FromBody]Registration registration)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
                 {
                     return BadRequest(ModelState);
                 }
+
+                string salt = PasswordFactory.GenerateSalt(128);
+                Entities.User user = new Entities.User()
+                {
+                    FirstName = registration.FirstName,
+                    MiddleName = registration.MiddleName,
+                    LastName = registration.LastName,
+                    Birthday = registration.Birthday,
+                    Gender = registration.Gender,
+                    Username = registration.Username,
+                    PasswordHash = PasswordFactory.Hash(registration.Password, salt),
+                    PasswordSalt = salt,
+                    UTCRegistrationDate = DateTime.UtcNow
+                };
+                await this._usersRepository.AddAsync(user, true);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //TODO: SaveException
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult IsUsernameAvailable(string id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                return Ok(this._usersRepository.FindByUsername(id) == null);
             }
             catch (Exception ex)
             {
@@ -53,7 +112,7 @@ namespace ESChatServer.Areas.v1.Controllers
             }            
         }
         [HttpGet]
-        public async Task<IActionResult> GetAsync()
+        public async Task<IActionResult> IsUsernameAvailableAsync(string id)
         {
             try
             {
@@ -62,68 +121,7 @@ namespace ESChatServer.Areas.v1.Controllers
                     return BadRequest(ModelState);
                 }
 
-                ICollection<Entities.User> result = await this._usersRepository.FindAllAsync();
-                if (result != null)
-                {
-                    return Ok(result);
-                }
-                else
-                {
-                    return BadRequest(ModelState);
-                }
-            }
-            catch (Exception ex)
-            {
-                //TODO: SaveException
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-
-        [HttpGet]
-        public IActionResult Detail(long id)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-
-                Entities.User result = this._usersRepository.Find(id);
-                if (result != null)
-                {
-                    return Ok(result);
-                }
-                else
-                {
-                    return BadRequest(ModelState);
-                }
-            }
-            catch (Exception ex)
-            {
-                //TODO: SaveException
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
-        [HttpGet]
-        public async Task<IActionResult> DetailAsync(long id)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-
-                Entities.User result = await this._usersRepository.FindAsync(id);
-                if (result != null)
-                {
-                    return Ok(result);
-                }
-                else
-                {
-                    return BadRequest(ModelState);
-                }
+                return Ok(await this._usersRepository.FindByUsernameAsync(id) == null);
             }
             catch (Exception ex)
             {
---
+++ ESChatServer/Areas/v1/Models/Application/Entities/Registration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESChatServer.Areas.v1.Models.Application.Entities
+{
+    public class Registration
+    {
+        [MaxLength(64), Required]
+        public string FirstName { get; set; }
+        [MaxLength(64)]
+        public string MiddleName { get; set; }
+        [MaxLength(64), Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+        [MaxLength(1), Required]
+        public string Gender { get; set; }
+
+        [MaxLength(64), Required]
+        public string Username { get; set; }
+        [MaxLength(128), Required]
+        public string Password { get; set; }
+    }
+}
